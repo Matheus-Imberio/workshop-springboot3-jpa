448f22ca0a741c29fa88d0fc60507ece681d32c3
Matheus-Imberio <matheusimberio030@gmail.com>
2024-07-12 16:35:14 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -14,37 +14,38 @@ import jakarta.persistence.Table;
 @Table(name = "tb_order_item")
 public class OrderItem implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@EmbeddedId
 	private OrderItemPK id = new OrderItemPK();
-	
+
 	private Integer quantity;
 	private Double price;
-	
+
 	public OrderItem() {
-		
+
 	}
 
-	public OrderItem(Order order, Product product,Integer quantity, Double price) {
+	public OrderItem(Order order, Product product, Integer quantity, Double price) {
 		super();
 		id.setOrder(order);
 		id.setProduct(product);
 		this.quantity = quantity;
 		this.price = price;
 	}
+
 	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
-	
+
 	public void setOrder(Order order) {
 		id.setOrder(order);
 	}
-	
+
 	public Product getProduct() {
 		return id.getProduct();
 	}
-	
+
 	public void setProduct(Product product) {
 		id.setProduct(product);
 	}
@@ -65,6 +66,10 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 
+	public Double getSubTotal() {
+		return quantity * price;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -81,6 +86,5 @@ public class OrderItem implements Serializable {
 		OrderItem other = (OrderItem) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
+
 }
